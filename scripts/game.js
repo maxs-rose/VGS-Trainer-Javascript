1a4c69452abfe2f33748a6712aaea309159e88b3
Max <max.rose366@gmail.com>
2020-12-27 19:12:56 +0000

    finished

---
+++ scripts/game.js
@@ -0,0 +1,118 @@
+var vgsJson = {}
+var selectableVGS = []
+var challenge = {}
+var input = ""
+var found = true
+var searchResults = []
+var score = 0
+
+$(document).ready(function()
+{
+    $.getJSON('../data/vgs.json', function(data) {
+        vgsJson = data
+
+        selectableVGS = []
+        vgsJson.forEach(function(element) {
+            if(element.final)
+                selectableVGS.push(element)
+        });
+
+        getChallenge()
+    });
+});
+
+$(document).keypress(function(e)
+{
+    if(e.which == 13)
+    {
+        score = 0
+        getChallenge()
+    }
+    else if(e.which == 32)
+    {
+        score = 0
+        clear()
+    }
+    else
+    {
+        input += e.key.toUpperCase()
+        updateInput()
+        getOptions(input)
+
+        if(found)
+            win()
+        else
+            updateDisplay()
+    }
+})
+
+function getOptions(comb)
+{
+    searchResults = []
+
+    vgsJson.forEach(function(vgs)
+    {
+        if(vgs.combo === comb.toLowerCase() && vgs.final === true)
+            searchResults.push(vgs)
+    })
+
+    if(searchResults.length == 1)
+    {
+        if(searchResults[0].combo === challenge.combo)
+        {
+            found = true
+            return
+        }
+        else
+            return
+    }
+    
+    searchResults = []
+
+    vgsJson.forEach(function(vgs)
+    {
+        if(vgs.combo.length === comb.length + 1 && vgs.combo.startsWith(comb.toLowerCase()))
+            searchResults.push(vgs)
+    })
+}
+
+function clear()
+{
+    input = ""
+    $("#user-input").html("Update Input")
+    $("#options-list").html("")
+    $("#score").find("span").html(score)
+}
+
+function updateInput()
+{
+    $("#user-input").html(input)
+}
+
+function getChallenge()
+{
+    found = false
+    clear()
+    challenge = selectableVGS[Math.floor(Math.random() * selectableVGS.length)]
+
+    $("#challenge").find("span").html(challenge.value)
+}
+
+function win()
+{
+    score++
+
+    $("#user-input").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    getChallenge()
+}
+
+function updateDisplay()
+{
+    $("#options-list").html("")
+
+    searchResults.forEach(res =>
+        {
+            $("#options-list").append(`<li>${res.combo.toUpperCase()} - ${res.value}</li>`)
+        })
+}
